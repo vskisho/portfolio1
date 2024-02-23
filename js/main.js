bdf6c59a147949ed66c57934054f1aa654b7964c
Vijaya Kumar KishoKumar <kisho.tech@floragroup.net>
2024-02-24 00:15:01 +0300

    First page upto 23:14

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".text",{
+    strings:["Web Developer","Back-End Developer","Full-Stack Developer","MicroService Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
